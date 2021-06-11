106f3ac02093f63677e41989a1180a2940cfc9e0
Sasitha Dilshan <sasithadilshan77@gmail.com>
2021-06-11 11:44:56 +0530

    Updated UI

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -12,77 +12,55 @@ class _PrintScreenState extends State<PrintScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Expanded(
+          Container(
+            height: 400.0,
             child: Stack(
-              children: [
+              children: <Widget>[
                 Container(
+                  height: 300,
                   padding: EdgeInsets.only(
                     bottom: 35,
                   ),
                   decoration: BoxDecoration(
                     color: Color(0xFFC21230),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(40.0),
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(30.0),
+                      bottomRight: Radius.circular(30.0),
                     ),
                   ),
                   child: Center(
                     child: Text(
                       'CRYPTO TRACKER',
                       style: TextStyle(
-                        // color: Color(0xFFF39B1A),
-                        fontSize: 40.0,
+                        fontSize: 30.0,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
                 ),
                 Positioned(
-                  bottom: 0,
-                  left: 0,
-                  right: 0,
+                  bottom: 60,
+                  left: 25,
+                  right: 25,
                   child: Container(
-                    margin: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-                    height: 70,
-                    child: Column(
-                      children: [
-                        Card(
-                          color: Color(0xFF6F172E),
-                          elevation: 5.0,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20.0),
-                          ),
-                          child: Center(
-                            child: Text(
-                              '1 BTC = ? USD',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                fontSize: 25.0,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
+                    height: 80,
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.all(Radius.circular(30.0)),
                     ),
                   ),
-                )
+                ),
               ],
             ),
           ),
-          Expanded(
-            child: Container(),
-          ),
-          Expanded(
-            child: Container(
-              height: 100.0,
-              alignment: Alignment.center,
-              padding: EdgeInsets.only(bottom: 30.0),
-              color: Color(0xFFC21230),
-              child: null,
-            ),
+          Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.only(bottom: 300.0),
+            color: Color(0xFFC21230),
+            child: null,
           ),
         ],
       ),
